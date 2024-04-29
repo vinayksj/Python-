026e6f139cdfcce7b254cd75fe32c0bf25a2251a
vinayksj <115333707+vinayksj@users.noreply.github.com>
2024-04-29 09:42:38 +0530

    print pattern7

---
+++ pattern7.py
@@ -0,0 +1,13 @@
+n = int(input())
+spaces = 0
+
+
+for row in range(n, 0, -1):
+   for space in range(spaces):
+       print(" ", end = "")
+      
+   spaces += 1
+  
+   for star in range(2 * row - 1):
+       print("*", end = "")
+   print()
